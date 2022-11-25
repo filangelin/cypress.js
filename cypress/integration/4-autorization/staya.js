761f72542fbdd372637983b0b4a2371bb560c230
Angelina <38373766+filangelin@users.noreply.github.com>
2022-11-25 19:40:59 +0300

    Update staya.js

--- cypress/integration/4-autorization/staya.js
+++ cypress/integration/4-autorization/staya.js
@@ -12,16 +12,4 @@ describe('Тестирование staya', function () {
 
          })
 
-         it('Авторизация', function () {
-            cy.visit('https://staya.dog/');
-            cy.get("#__layout > div > div > section > div > div > article > button").click()
-            cy.get('.header-bottom__right--link').click()
-            cy.get('.auth-form > form > [type="email"]').type('lina.filimonova.1999@mail.ru')
-            cy.get('.auth-form > form > [type="password"]').type('Staya')
-            cy.get('.auth-form__submit').click()
-            cy.contains("Мои заказы")
-            cy.end
-    
-             })
-      
 })
